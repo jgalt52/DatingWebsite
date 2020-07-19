d2fe388d97643bce004baa72014215ed6b0142b0
jgalt52 <hwaggone@uccs.edu>
2020-07-19 13:11:41 -0600

    Token Handling bootswatch theme 3rd party componen

--- DatingWebsite-SPA/src/app/app.module.ts
+++ DatingWebsite-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppComponent } from './app.component';
 
@@ -10,6 +11,7 @@ import {AuthService} from './_services/auth.service';
 import {ErrorInterceptorProvider} from './_services/error.interceptor';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -22,7 +24,9 @@ import { RegisterComponent } from './register/register.component';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BrowserAnimationsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
