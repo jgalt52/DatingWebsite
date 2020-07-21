a56482f5d221f4264f5a2cd1fb1103d68dc4db0a
jgalt52 <hwaggone@uccs.edu>
2020-07-21 14:19:27 -0600

    Added angular routing and route guard

--- DatingWebsite-SPA/src/app/app.module.ts
+++ DatingWebsite-SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 
@@ -12,6 +13,11 @@ import {ErrorInterceptorProvider} from './_services/error.interceptor';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import {appRoutes} from './routes';
+
 
 
 @NgModule({
@@ -19,19 +25,22 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       AppComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BrowserAnimationsModule,
-      BsDropdownModule.forRoot()
+      BsDropdownModule.forRoot(),
+      RouterModule.forRoot(appRoutes)
    ],
    providers: [
       AuthService,
       ErrorInterceptorProvider
-
    ],
    bootstrap: [
       AppComponent
